13561306f7697cef7fba339d73bd2281dea4dadb
Hugo-Lourenco <hugo.lourenco@ynov.com>
2026-05-07 11:32:49 +0200

    main.js with the base of the application

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+  // Create the window
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false 
+    }
+  });
+
+  
+  win.loadFile(path.join(__dirname, 'renderer', 'index.html'));
+}
+
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// quit when all the windows are shut
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
